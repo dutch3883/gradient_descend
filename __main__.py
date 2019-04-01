cbf06fc8198e4cfccf25457beced5d8b23b770c3
dutch3883 <dutch3883@hotmail.com>
2019-04-02 01:47:58 +0700

    update local

--- __main__.py
+++ __main__.py
@@ -1,26 +1,36 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
-
+import math
 
 def get_data():
     points =[]
     with open('data.csv','r') as file:
         for i in file.readlines():
-            points.append([float(x) for x in i.split(',')])
-    return points
+            points.append({'x':float(i.split(',')[0]),'y': float(i.split(',')[1])})
+            points = sorted(points,key=lambda a:a['x'])
+    ret_dat = []
+    for i in points:
+        ret_dat.append([i['x'],i['y']])
+    print(ret_dat)
+    return ret_dat
+
+
+
 ax1=None
 fig=None
-def ploter(m,b,data):
+
+def ploter(f,data):
     time.sleep(0.001)
     xs = [i[0] for i in data]
     ys = [i[1] for i in data]
+
     global  ax1
     global fig
     forecast_line_y = []
     first_time = False
     for x in xs:
-        forecast_line_y.append(m*x+b)
+        forecast_line_y.append(f(x))
     if ax1 is None:
 
         first_time =True
@@ -38,17 +48,30 @@ def ploter(m,b,data):
     else:
         fig.canvas.draw()
 
-def cal_a_point_error(point,m,b):
+def cal_a_point_error(point,f):
     x = point[0]
     y = point[1]
-    error = (y - (m*x+b))**2
+    error = (y - f(x))**2
     return error
 
-def cal_avg_error_for_points(points,m,b):
+def cal_avg_error_for_points(points,f):
     total_error =0
     for i in points:
-        total_error+=cal_a_point_error(i,m,b)
+        total_error+=cal_a_point_error(i,f)
     return total_error/len(points)
+
+def partial_diff_quadratic(points,theta,f,learning_rate):
+    N = len(points)
+    diff_theta = [0]*len(theta)
+    for i in points:
+        x = i[0]
+        y = i[1]
+        for i in range(0,len(diff_theta)):
+            diff_theta[i] += -2 / N * (y - (f(x))) * math.pow(x,i)
+
+    for i in range(0,len(theta)):
+        theta[i] = theta[i] - diff_theta[i] * learning_rate
+    return theta
 
 def partial_diff_m(points,m,b,learning_rate):
     N =  len(points)
@@ -63,21 +86,35 @@ def partial_diff_m(points,m,b,learning_rate):
     new_b  = b - diff_b*learning_rate
     return new_m, new_b
 
-def descent_gradient(data,init_m,init_b,iteration):
-    b= init_b
-    m = init_m
-    learning_rate = 1
-    error = 0
-    for i in range(iteration):
-        m,b = partial_diff_m(data,m=m,b=b,learning_rate=learning_rate)
-        new_error = cal_avg_error_for_points(data,m=m,b=b)
-        if error != 0 and new_error -error > 0:
-            learning_rate/=5
-        print('iteration {} : error = {} change = {} learning rate = {}'.format(i+1,new_error,new_error-error,learning_rate))
-        ploter(m,b,data)
-        error = new_error
+def descent_gradient(data,dim,iteration):
+    learning_rate = 0.0001
+    theta = [0.030269517287775907, 1.478895662279729]
+    error =0
 
+    def fx(x):
+        accum = 0
+        for i in range(0, len(theta)):
+            accum += theta[i] * math.pow(x, i)
+        return accum
+
+    for i in range(iteration):
+        theta = partial_diff_quadratic(data,theta,fx,learning_rate=learning_rate)
+
+        def fx(x):
+            accum =0
+            for i in range(0,len(theta)):
+                accum += theta[i]*math.pow(x,i)
+            return accum
+
+        new_error = cal_avg_error_for_points(data,fx)
+        if error != 0 and new_error -error > 0:
+            learning_rate/=10
+        print('iteration {} : error = {} change = {} learning rate = {}'.format(i+1,new_error,new_error-error,learning_rate))
+
+        ploter(fx,data)
+        error = new_error
+    return theta
 
 if __name__  == "__main__":
     g = get_data()
-    descent_gradient(g,1,0,10000)
+    print(descent_gradient(g,2,20))
